a0f0715d768a625416ef6d10886de20465e98c49
Aayush Karna <aayushkarna6191@gmail.com>
2021-10-02 15:58:36 +0300

    responsive

--- js/script.js
+++ js/script.js
@@ -1,18 +1,50 @@
+// make mobile navigation work
+const btnNavEl = document.querySelectorAll('.btn-mobile-nav');
+const body = document.querySelector('body');
+const porfileContent = document.querySelector('.profile-content');
+
+btnNavEl.forEach(el =>
+  el.addEventListener('click', () => {
+    body.classList.toggle('nav-open');
+  })
+);
+
+// sticky navigation
+// const obs = new IntersectionObserver(
+//   entries => {
+//     const ent = entries[0];
+
+//     if (!ent.isIntersecting) {
+//       bodyEl.classList.add('sticky');
+//     }
+
+//     if (ent.isIntersecting) {
+//       bodyEl.classList.remove('sticky');
+//     }
+//   },
+//   {
+//     root: null, // in the view port
+//     threshold: 0,
+//     rootMargin: '-8px'
+//   }
+// );
+// obs.observe(porfileContent);
+
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
-  var flex = document.createElement("div");
-  flex.style.display = "flex";
-  flex.style.flexDirection = "column";
-  flex.style.rowGap = "1px";
+  var flex = document.createElement('div');
+  flex.style.display = 'flex';
+  flex.style.flexDirection = 'column';
+  flex.style.rowGap = '1px';
 
-  flex.appendChild(document.createElement("div"));
-  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement('div'));
+  flex.appendChild(document.createElement('div'));
 
   document.body.appendChild(flex);
   var isSupported = flex.scrollHeight === 1;
   flex.parentNode.removeChild(flex);
   console.log(isSupported);
 
-  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+  if (!isSupported) document.body.classList.add('no-flexbox-gap');
 }
 checkFlexGap();
